7071ae947c0f196d03c99efe04d3bc9d307eb86b
Shuang Li <shuang_li@ucsb.edu>
2025-04-23 19:59:22 -0700

    sl - add GET and tests on articles

--- src/main/java/edu/ucsb/cs156/example/controllers/ApiController.java
+++ src/main/java/edu/ucsb/cs156/example/controllers/ApiController.java
@@ -1,13 +1,19 @@
 package edu.ucsb.cs156.example.controllers;
 
+import edu.ucsb.cs156.example.entities.UCSBDate;
 import edu.ucsb.cs156.example.errors.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import edu.ucsb.cs156.example.models.CurrentUser;
 import edu.ucsb.cs156.example.services.CurrentUserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.Map;
@@ -51,4 +57,6 @@ public abstract class ApiController {
       "message", e.getMessage()
     );
   }
+
+
 }
